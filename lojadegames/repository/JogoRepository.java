5e102634a517f83c3848aae20eeb114023d27fc9
Beatriz Moreli <79378960+Beamoreli@users.noreply.github.com>
2022-03-31 16:11:09 -0300

    Update JogoRepository.java

--- lojadegames/repository/JogoRepository.java
+++ lojadegames/repository/JogoRepository.java
@@ -5,7 +5,7 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.generation.lojadegames.model.Jogo;
-
+@Repository
 public interface JogoRepository extends JpaRepository<Jogo, Long>{
 	public List<Jogo> findAllByNomeContainingIgnoreCase(String nome);
 }
